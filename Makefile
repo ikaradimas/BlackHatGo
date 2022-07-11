5fb3fb0457a964a4bd6e519dbfde5ae0cfda150a
Ioannis Karadimas <ikaradimas@hotmail.com>
2022-07-11 20:56:10 +0300

    Echo server implementation

--- Makefile
+++ Makefile
@@ -3,4 +3,7 @@
 .rude_scanner: 
 	go build -o bin/rude_scanner cmd/rude_scanner/main.go
 
-all: .rude_scanner
+.echo_server:
+	go build -o bin/echo_server cmd/echo_server/main.go
+
+all: .rude_scanner .echo_server
--- cmd/rude_scanner/main.go
+++ cmd/rude_scanner/main.go
@@ -3,16 +3,49 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
-func main() {
-	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+func worker(ports chan int, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		fmt.Print(".")
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
+			results <- 0
 			continue
 		}
 		conn.Close()
-		fmt.Printf("%d: open\n", i)
+		results <- p
+	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i <= 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
 	}
 }
